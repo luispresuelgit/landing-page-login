d64fcd9a28ca350a4e6db59924f32bf3725d4ea1
Luis Presuel <luis.presuel@aluxoft.com>
2017-12-11 20:12:35 -0600

    third commit

--- login.js
+++ login.js
@@ -1,8 +1,35 @@
-$('#myForm').submit(function() {
-  event.preventDefault();
-  var usrn = $('#shopper-login').children('input[name="email"]');
-  var pwd = $('#password-yui_3_17_2_1_1509249008972_105948-field').val();
-  console.log(usrn);
-  console.log(pwd);
-            
-}
+$(document).ready(function() {
+    $('#shopper-login').submit(function() {
+        event.preventDefault();
+        var usrn = $('#loginForm').children('input[name="username"]').val();
+        var pwd = $('#loginForm').children('input[type="password"]').val();
+
+        $.ajax({
+                    type: "POST",
+                    url: 'http://qick.co:8081/auth/login', //ruta absoluta
+                    data: JSON.stringify({username:usrn,password:pwd}),
+                    dataType: "json",
+                    contentType: "application/json",
+
+                    success: function (data) {
+                        if (data == undefined) {
+                            alert("Error : 219");
+                        }
+                        else{
+                          alert("Login successful");
+                            Cookies.set('token', data);
+                            Cookies.set('username',usrn);
+
+                        }
+                    },
+                    error: function (data) {
+                        if (data == undefined) {
+                            alert("Error : 465");
+                        }
+                        else {
+                            $("#alert-box").show();
+                        }
+                    }
+        });
+    });
+});
