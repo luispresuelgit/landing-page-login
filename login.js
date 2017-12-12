c2b2c0554e4060987a2559f589c93c9936adf004
Luis Presuel <luis.presuel@aluxoft.com>
2017-12-11 19:01:24 -0600

    second commit

--- login.js
+++ login.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo");
+$('#myForm').submit(function() {
+  event.preventDefault();
+  var usrn = $('#shopper-login').children('input[name="email"]');
+  var pwd = $('#password-yui_3_17_2_1_1509249008972_105948-field').val();
+  console.log(usrn);
+  console.log(pwd);
+            
+}
