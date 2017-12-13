12b56a605fd0bc276684d042824cf1d3f8f969d5
Luis Presuel <luis.presuel@aluxoft.com>
2017-12-13 10:56:12 -0600

    Modified form js to match submit form

--- js/userRegister.js
+++ js/userRegister.js
@@ -1,42 +1,48 @@
+$(document).ready(function() {
+    $('form').submit(function(event) {
+      event.preventDefault();
+      var firstName_user = $('form :input[name=fname]').val();
+      var lastName_user = $('form :input[name=lname]').val();
+      var userName_user = $('form :input[name=email]').val();
+      var password_user = $('form :input[type=password]').val();
+      var password_confirm = $('form :input[id=text-yui_3_17_2_1_1509417288714_89069-field]').val();
+      if(password_user == password_confirm){
+        //Sign up user
+        $.ajax({
+                    type: "POST",
+                    url: 'https://qick.co:8443/auth/login', //Absolute route
+                    data: JSON.stringify({
+                      firstname:firstName_user,
+                      lastname:lastName_user,
+                      username:userName_user,
+                      password:password_user
+                    }),
+                    dataType: "json",
+                    contentType: "application/json",
 
-function userRegistration(){
+                    success: function (data) {
+                        if (data == undefined) {
+                            alert("Error : 219");
+                        }
+                        else{
+                            loginUser(data['username'], password_user);
+                        }
+                    },
+                    error: function (data) {
+                        if (data == undefined) {
+                            alert("Error : 465");
+                        }
+                        else {
+                            alert("Error : 466");
+                        }
+                    }
+                });
+        }else{
+          alert("Passwords don't match. Try again");
+        }
+    });
+});
 
-  var firstName_user = $("#firstName").val();
-  var lastName_user = $("#lastName").val();
-  var userName_user = $("#username").val();
-  var password_user = $("#password").val();
-
-  //Sign up user
-  $.ajax({
-              type: "POST",
-              url: 'https://qick.co:8443/auth/login', //Absolute route
-              data: JSON.stringify({
-                firstname:firstName_user,
-                lastname:lastName_user,
-                username:userName_user,
-                password:password_user
-              }),
-              dataType: "json",
-              contentType: "application/json",
-
-              success: function (data) {
-                  if (data == undefined) {
-                      alert("Error : 219");
-                  }
-                  else{
-                      loginUser(data['username'], password_user);
-                  }
-              },
-              error: function (data) {
-                  if (data == undefined) {
-                      alert("Error : 465");
-                  }
-                  else {
-                      alert("Error : 466");
-                  }
-              }
-          });
-}
 function loginUser(usrn, pwd){
           $.ajax({
                       type: "POST",
