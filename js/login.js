4afd64c90811d225f86856b3a476121d5253fe7d
Luis Presuel <luis.presuel@aluxoft.com>
2017-12-13 09:48:12 -0600

    Revert "Register button test2"
    This reverts commit 1cf285b00d2be36065f12fc0f48d4c5552dd435d.

--- js/login.js
+++ js/login.js
@@ -33,9 +33,8 @@ $(document).ready(function() {
         });
     });
 
-    $("a.sqs-block-button-element--medium.sqs-block-button-element").get(0).click(function(event){
+    $("a.sqs-block-button-element--medium.sqs-block-button-element").click(function(event){
       event.preventDefault();
-      var url ="/shop2";
       location.href = "https://www.google.com";
       }
     );
