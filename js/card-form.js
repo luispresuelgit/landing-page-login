95c640dc48154dd6020046a0ef61072aa8cb671b
Luis Presuel <luis.presuel@aluxoft.com>
2018-04-09 19:57:08 -0500

    Fixes route for submitting card into server

--- js/card-form.js
+++ js/card-form.js
@@ -201,7 +201,7 @@ function getStripePublicKey(){
 
             $.ajax({
               type: "POST",
-              url: 'https://qick.co:8443/cards?token_id='+cardToken['id'],
+              url: QCS+'/cards?token_id='+cardToken['id'],
               headers: {
                 Authorization: 'Bearer '+sessionToken['token']
               },
